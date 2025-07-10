160acb5080e5cd26aad12dbaa5079582cb2e868a
Behzad Ehsani <ehsani.eof@gmail.com>
2025-07-10 23:42:35 +0330

    restart

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/behzade/basalt
+
+go 1.24.4
---
+++ token/token.go
@@ -0,0 +1,77 @@
+package token
+
+// TokenType is a string to allow for flexibility and easy debugging.
+type TokenType string
+
+// Token represents a single token with its type and literal value.
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// Defines all possible token types in our language.
+const (
+	// Special Tokens
+	ILLEGAL = "ILLEGAL" // A token/character we don't know about
+	EOF     = "EOF"     // "End of File"
+
+	// Identifiers + Literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	MODULO   = "%"
+	POW      = "**"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+	MUT       = "MUT"
+	TRUE      = "TRUE"
+	FALSE     = "FALSE"
+	IF        = "IF"
+	ELSE      = "ELSE"
+	RETURN    = "RETURN"
+	STRUCT    = "STRUCT"
+	INTERFACE = "INTERFACE"
+)
+
+// keywords maps keyword strings to their TokenType.
+var keywords = map[string]TokenType{
+	"fn":        FUNCTION,
+	"let":       LET,
+	"mut":       MUT,
+	"true":      TRUE,
+	"false":     FALSE,
+	"if":        IF,
+	"else":      ELSE,
+	"return":    RETURN,
+	"struct":    STRUCT,
+	"interface": INTERFACE,
+}
+
+// LookupIdent checks the keywords table to see if an identifier is a keyword.
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
